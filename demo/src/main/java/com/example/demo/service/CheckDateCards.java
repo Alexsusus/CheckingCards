49b0efd746747f46a3ed13aab00e5cae7fc16ed3
Alex <gopazz098@gmail.com>
2023-01-12 19:03:42 +0200

    init, home, entity, addCard, CheckDateCards, change bank to number, unique

--- demo/src/main/java/com/example/demo/service/CheckDateCards.java
+++ demo/src/main/java/com/example/demo/service/CheckDateCards.java
@@ -10,13 +10,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
